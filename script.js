bc64f9aba448ec495794c96d3047e8cc5a9c6d69
moarifur <moarifur@gmail.com>
2024-06-23 20:53:12 +0600

    Some issues has been solved!

---
+++ script.js
@@ -0,0 +1,20 @@
+function sendMail() {
+    let params = {
+        name: document.getElementById('name').value,
+        age: document.getElementById('age').value,
+        parent: document.getElementById('parent').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+    }
+
+    emailjs.send("service_shwi2bt", "template_sqjehl6", params).then(
+        (response) => {
+            alert('Email has been sent successfully')
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            alert('Email has not been sent!')
+            console.log('FAILED...', error);
+        },
+    )
+}
