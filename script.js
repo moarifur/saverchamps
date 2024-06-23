103bf8365c32a0dfa58678297ba2a0277afa77dc
moarifur <moarifur@gmail.com>
2024-06-23 21:35:14 +0600

    Contact Form Reset after clickin OK

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function sendMail() {
+    let frm = document.getElementById('contact-form')
     let params = {
         name: document.getElementById('name').value,
         age: document.getElementById('age').value,
@@ -7,17 +8,37 @@ function sendMail() {
         phone: document.getElementById('phone').value,
     }
 
-    emailjs.send("service_shwi2bt", "template_sqjehl6", params).then(
-        (response) => {
-            alert('Email has been sent successfully')
-            console.log('SUCCESS!', response.status, response.text);
-        },
-        (error) => {
-            alert('Email has not been sent!')
-            console.log('FAILED...', error);
-        },
-    )
-
-    let frm = document.getElementById('contact-form')
-    frm.reset()
+    emailjs.send("service_shwi2bt", "template_sqjehl6", params)
+        .then(alert('Email has been sent successfully'))
+        .then(frm.reset())
 }
+
+
+
+
+
+
+
+
+// function sendMail() {
+//     let frm = document.getElementById('contact-form')
+//     let params = {
+//         name: document.getElementById('name').value,
+//         age: document.getElementById('age').value,
+//         parent: document.getElementById('parent').value,
+//         email: document.getElementById('email').value,
+//         phone: document.getElementById('phone').value,
+//     }
+//
+//     emailjs.send("service_shwi2bt", "template_sqjehl6", params).then(
+//         (response) => {
+//             alert('Email has been sent successfully')
+//             console.log('SUCCESS!', response.status, response.text);
+//         },
+//         (error) => {
+//             alert('Email has not been sent!')
+//             console.log('FAILED...', error);
+//         },
+//     )
+//     frm.reset()
+// }
